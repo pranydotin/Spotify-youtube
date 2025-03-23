67d3d43fc6f8aa47f2d49d330864b88960714640
Pranay <npdotin@gmail.com>
2025-03-23 19:12:55 +0530

    added youtube access token as well

--- accessToken.py
+++ accessToken.py
@@ -3,12 +3,12 @@ import base64
 import pandas as pd
 
 
-def getAccessToken():
+def getSpotifyAccessToken():
     # get the details
     data = pd.read_json("./details.json")
-    refresh = data.refresh_token[0]
-    client_id = data.client_id[0]
-    client_secret = data.client_secret[0]
+    refresh = data.spotify.refresh_token
+    client_id = data.spotify.client_id
+    client_secret = data.spotify.client_secret
 
     credentials = base64.b64encode(
         f"{client_id}:{client_secret}".encode()).decode()
@@ -23,5 +23,25 @@ def getAccessToken():
 
     response = requests.request("POST", url, headers=headers, data=payload)
 
+    response = response.json()
+    return response.get('access_token')
+
+
+def getYoutubeAccessToken():
+
+    data = pd.read_json("./details.json")
+    refresh = data.youtube.refresh_token
+    client_id = data.youtube.client_id
+    client_secret = data.youtube.client_secret
+
+    url = "https://oauth2.googleapis.com/token"
+
+    payload = f'client_id={client_id}&client_secret={client_secret}&redirect_uri=https%3A%2F%2Fwww.example.com&grant_type=refresh_token&refresh_token={refresh}'
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
     response = response.json()
     return response.get('access_token')
